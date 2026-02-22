5e09060471c41de6b1cf69c552d966cb18dc3e31
Gileno <gilenoduarte.jobs@gmail.com>
2026-02-22 13:48:28 -0300

    fix: Add isSorted varible to verify if the array is already sorted

--- sorting-algorithms/buble_sorting/main.go
+++ sorting-algorithms/buble_sorting/main.go
@@ -6,17 +6,23 @@ func Buble(nums []int) {
 	size := len(nums)
 
 	for idx, _ := range nums {
+		isSorted := true
 		fmt.Println(idx, nums)
 		for i := 0; i < (size - 1); i++ {
 			tmp := nums[i]
 			if nums[i] > nums[i+1] {
+				isSorted = false
 				nums[i] = nums[i+1]
 				nums[i+1] = tmp
 			}
+		}
+		if isSorted {
+			return
 		}
 	}
 }
 
 func main() {
 	Buble([]int{5, 4, 3, 2, 1})
+	Buble([]int{1, 2, 3, 4, 5})
 }
